e1e889e954669c5d0edca385c2d24b5f384b837d
astropsy999 <bareust@gmail.com>
2023-05-07 12:00:48 +0300

    sessions-cookies

--- src/pages/api/completion.js
+++ src/pages/api/completion.js
@@ -1,4 +1,5 @@
 import { Configuration, OpenAIApi } from 'openai';
+import { withNextSession } from '../../lib/session';
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -9,7 +10,7 @@ const AI_PROMPT =
 const AI_RESPONSE =
   "```js\nimport React from 'react';\n\nconst MyComponent = () => {\n  return <div>I'm a simple component!</div>;\n};\n\nexport default MyComponent;\n```\n\nThis example is a basic React component. It imports the React library, defines a component function, and returns a DOM element. Finally, the component is exported so it can be imported and used in other components.";
 
-export default async function completion(req, res) {
+export default withNextSession(async (req, res) => {
   if (req.method === 'POST') {
     const body = req.body;
     const prompt = body.prompt || '';
@@ -35,7 +36,23 @@ export default async function completion(req, res) {
       console.log(e.message);
       return res.status(500).json({ error: { message: e.message } });
     }
+  } else if (req.method === 'PUT') {
+    const { uid } = req.query;
+
+    if (!uid) {
+      return res
+        .status(500)
+        .json({ error: { message: 'Invalid uid provided!' } });
+    }
+
+    req.session.user = {
+      uid,
+    };
+
+    await req.session.save();
+
+    return res.status(200).json(uid);
   } else {
     return res.status(500).json({ error: { message: 'Invalid Api Route' } });
   }
-}
+});
--- src/pages/learning/[stack].js
+++ src/pages/learning/[stack].js
@@ -4,9 +4,17 @@ import Prompt from '@/components/Prompt';
 import stacks from '@/data/stack.json';
 import { useEffect, useRef, useState } from 'react';
 
+const SESSION_KEYS = [
+  'u1-2023-04-13T15:36:20.424Z',
+  'u2-2023-04-13T15:36:20.123Z',
+  'u3-2023-04-13T15:36:20.421Z',
+  'u4-2023-04-13T15:36:20.999Z',
+];
+
 export default function Stack({ stack, stackKey }) {
   const [messages, setMessages] = useState([]);
   const chatRef = useRef(null);
+  const [activeSession, setActiveSession] = useState('');
 
   useEffect(() => {
     chatRef.current.scrollTo(0, chatRef.current.scrollHeight);
@@ -28,7 +36,7 @@ export default function Stack({ stack, stackKey }) {
       ];
     });
 
-    const response = await fetch('/api/completition', {
+    const response = await fetch('/api/completion', {
       method: 'POST',
       body: JSON.stringify({ prompt }),
       headers: {
@@ -52,9 +60,32 @@ export default function Stack({ stack, stackKey }) {
       console.error(json?.error?.message);
     }
   };
+  const handleSessionChange = async (e) => {
+    const session = e.target.value;
+
+    if (!session) {
+      console.log('Not valid session!');
+      return;
+    }
+
+    await fetch(`/api/completion?uid=${session}`, { method: 'PUT' });
+    setActiveSession(session);
+  };
+
   return (
     <div className="h-full flex flex-col">
       <Header logo={stack.logo} info={stack.info} />
+      <select
+        onChange={handleSessionChange}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-[200px] p-2.5 mt-5"
+      >
+        <option value={''}>Choose session</option>
+        {SESSION_KEYS.map((sk) => (
+          <option key={sk} value={sk}>
+            {sk}
+          </option>
+        ))}
+      </select>
       <hr className="my-4" />
       <div ref={chatRef} className="chat flex flex-col h-full overflow-auto">
         {messages.map((message, i) => (
